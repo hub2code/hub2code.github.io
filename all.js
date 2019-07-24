7bfc9910e91bce5394a14a7ca89afc29a1e0af9d
hub2code <48771656+hub2code@users.noreply.github.com>
2019-07-24 12:07:02 +0300

    Update all.js

--- all.js
+++ all.js
@@ -129,11 +129,11 @@ window.addEventListener("DOMContentLoaded", function() {
 //         y += 90;
 //         title.innerHTML = "работает ориентация" + x + "    <<<<x" + y + "    <<<<<<y";
  
-        img1.style.transform = `translate3d(${(x - centerX) * 1/100}px, ${(y - centerY) * 1/100}px, 0 )`;
-        img2.style.transform = `translate3d(${(x - centerX) * 2/100}px, ${(y - centerY) * 2/100}px, 0 )`;
-        img3.style.transform = `translate3d(${(x - centerX) * 3/100}px, ${(y - centerY) * 3/100}px, 0 )`;
-        img4.style.transform = `translate3d(${(x - centerX) * 4/100}px, ${(y - centerY) * 4/100}px, 0 )`;
-        img5.style.transform = `translate3d(${(x - centerX) * 20/100}px, ${(y - centerY) * 20/100}px, 0 )`;
+        img1.style.transform = `translate3d(${(y - centerX) * 1/100}px, ${(x - centerY) * 1/100}px, 0 )`;
+        img2.style.transform = `translate3d(${(y - centerX) * 2/100}px, ${(x - centerY) * 2/100}px, 0 )`;
+        img3.style.transform = `translate3d(${(y - centerX) * 3/100}px, ${(x - centerY) * 3/100}px, 0 )`;
+        img4.style.transform = `translate3d(${(y - centerX) * 4/100}px, ${(x - centerY) * 4/100}px, 0 )`;
+        img5.style.transform = `translate3d(${(y - centerX) * 20/100}px, ${(x - centerY) * 20/100}px, 0 )`;
 
     };
     function gyro2() {
